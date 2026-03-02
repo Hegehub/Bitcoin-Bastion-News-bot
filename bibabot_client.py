ef567fa5a4089cb4210916d977b771f45078ca1c
Hegehub <r7xtvnxdnf@privaterelay.appleid.com>
2026-03-02 21:53:03 +0300

    Replace sse_client import with aiohttp_sse_client

--- bibabot_client.py
+++ bibabot_client.py
@@ -2,7 +2,7 @@ import aiohttp
 import asyncio
 import logging
 from typing import Optional, Dict, List, Any
-from sse_client import EventSource  # используем aiohttp-sse-client
+from aiohttp_sse_client import client as sse_client
 
 logger = logging.getLogger(__name__)
 
