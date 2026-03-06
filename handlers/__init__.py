3b49c6ca65e904f1bee737226959b13c855d7e20
Hegehub <r7xtvnxdnf@privaterelay.appleid.com>
2026-03-06 21:02:43 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,4 @@
-from . import private, group, admin
+from .api_client import api_client
+from .cryptorank_client import cryptorank
+from .price_history import price_history
+from .trigger_detector import trigger_detector
