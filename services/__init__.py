8fc4740c83af47a601c6107bff5c89c716d6f6d2
Hegehub <r7xtvnxdnf@privaterelay.appleid.com>
2026-03-13 19:06:16 +0300

    Delete services directory

--- services/__init__.py
+++
@@ -1,5 +0,0 @@
-from .api_client import api_client as api_client
-from .trigger_detector import trigger_detector as trigger_detector
-from .price_history import price_history as price_history
-
-__all__ = ["api_client", "trigger_detector", "price_history"]
