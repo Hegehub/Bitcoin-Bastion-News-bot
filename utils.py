7caff666baadeced2292f62194e29ac2e737c918
Hegehub <r7xtvnxdnf@privaterelay.appleid.com>
2026-03-13 19:09:10 +0300

    Delete utils.py

--- utils.py
+++
@@ -1,5 +0,0 @@
-import html
-
-def escape_html(text: str) -> str:
-    """Экранирует спецсимволы для безопасного использования в HTML-разметке Telegram."""
-    return html.escape(text, quote=False)
